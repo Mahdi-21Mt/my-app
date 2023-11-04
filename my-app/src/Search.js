050ed878e462bfc53362a9d48b6a37968cef5382
Mahdi <m.tp654711@gmail.com>
2023-11-04 11:59:28 +0330

    1

--- my-app/src/Search.js
+++ my-app/src/Search.js
@@ -1,44 +1,44 @@
-import React, { useState, useEffect } from "react"; 
-import axios from "axios"; 
+// import React, { useState, useEffect } from "react"; 
+// import axios from "axios"; 
  
-const CardList = () => { 
-  const [Photos, setPhotos] = useState(null); 
+// const CardList = () => { 
+//   const [Photos, setPhotos] = useState(null); 
  
-  useEffect = 
+//   useEffect = 
    
-    (() => { 
-      axios 
-        .get("https://api.unsplash.com/search/photos?query=car", { 
-          // params:{query : 'car'}, 
-          headers: { 
-            Authorization: 
-'Client-ID MJB59FE8cICGSj8oLtDyavDAsnWNm9uXLBXe-1Oa6_w'          }, 
-        }) 
-        .then((Response) => { 
-          // console.log(Response.data.result); 
-          setPhotos(Response.data.result); 
+//     (() => { 
+//       axios 
+//         .get("https://api.unsplash.com/search/photos?query=car", { 
+//           // params:{query : 'car'}, 
+//           headers: { 
+//             Authorization: 
+// 'Client-ID MJB59FE8cICGSj8oLtDyavDAsnWNm9uXLBXe-1Oa6_w'          }, 
+//         }) 
+//         .then((Response) => { 
+//           // console.log(Response.data.result); 
+//           setPhotos(Response.data.result); 
          
-        }); 
-    }, 
-    []); 
+//         }); 
+//     }, 
+//     []); 
  
-        console.log(Photos); 
+//         console.log(Photos); 
  
-        const ImagesList = Photos.map(obj=>{ 
-          return( 
-            <Card/> 
-          ) 
+//         const ImagesList = Photos.map(obj=>{ 
+//           return( 
+//             <Card/> 
+//           ) 
  
-        }) 
+//         }) 
   
  
  
  
-  return ( 
-    <div> 
+//   return ( 
+//     <div> 
        
-    </div> 
-  ); 
-}; 
+//     </div> 
+//   ); 
+// }; 
  
-export default Search;
+// export default Search;
